c483ea43d7ec4646b5ebfd253ecdff43d6259b30
okmtyuta <okmtyuta@gmail.com>
2025-01-02 16:19:36 +0900

    initial commit

---
+++ client/src/main.rs
@@ -0,0 +1,64 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Header {}
+        Frame {}
+        Footer {}
+    }
+}
+
+#[component]
+pub fn Frame() -> Element {
+    rsx! {
+        div {
+            class: "frame",
+            "AAA"
+        }
+    }
+}
+
+#[component]
+pub fn Header() -> Element {
+    rsx! {
+        header {
+            class: "header",
+            div {
+                class: "header-content",
+                a {
+                    class: "logo",
+                    href: "/",
+                    "research.okmtyuta.phd"
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn Footer() -> Element {
+    rsx! {
+        footer {
+            class: "footer",
+            span {
+                "© 2024"
+            },
+            a {
+                class: "logo",
+                href: "/",
+                "research.okmtyuta.phd"
+            }
+        }
+    }
+}
