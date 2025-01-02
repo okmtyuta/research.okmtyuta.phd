9902ad93ec636c590594ca0028ae711a4a659a70
okmtyuta <okmtyuta@gmail.com>
2025-01-03 02:56:31 +0900

    fix

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,6 @@ use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
     dioxus::launch(App);
